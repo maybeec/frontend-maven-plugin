c2c053323cd7dc68ff714d4509cbe0d76bdb665d
polarctos <polarctos@users.noreply.github.com>
2016-10-28 21:47:59 +0200

    eirslett/frontend-maven-plugin#490 revert formatting changes; remove .buildenv folder usage; extended readme

--- frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/FrontendPluginFactory.java
+++ frontend-plugin-core/src/main/java/com/github/eirslett/maven/plugins/frontend/lib/FrontendPluginFactory.java
@@ -3,45 +3,39 @@ package com.github.eirslett.maven.plugins.frontend.lib;
 import java.io.File;
 
 public final class FrontendPluginFactory {
-
+    
     private static final Platform defaultPlatform = Platform.guess();
-
     private static final String DEFAULT_CACHE_PATH = "cache";
 
     private final File workingDirectory;
-
     private final File installDirectory;
-
     private final CacheResolver cacheResolver;
 
-    public FrontendPluginFactory(File workingDirectory, File installDirectory) {
+    public FrontendPluginFactory(File workingDirectory, File installDirectory){
         this(workingDirectory, installDirectory, getDefaultCacheResolver(installDirectory));
     }
 
-    public FrontendPluginFactory(File workingDirectory, File installDirectory, CacheResolver cacheResolver) {
+    public FrontendPluginFactory(File workingDirectory, File installDirectory, CacheResolver cacheResolver){
         this.workingDirectory = workingDirectory;
         this.installDirectory = installDirectory;
         this.cacheResolver = cacheResolver;
     }
 
     public NodeInstaller getNodeInstaller(ProxyConfig proxy) {
-        return new NodeInstaller(getInstallConfig(), new DefaultArchiveExtractor(),
-            new DefaultFileDownloader(proxy));
+        return new NodeInstaller(getInstallConfig(), new DefaultArchiveExtractor(), new DefaultFileDownloader(proxy));
     }
 
     public NPMInstaller getNPMInstaller(ProxyConfig proxy) {
-        return new NPMInstaller(getInstallConfig(), new DefaultArchiveExtractor(),
-            new DefaultFileDownloader(proxy));
+        return new NPMInstaller(getInstallConfig(), new DefaultArchiveExtractor(), new DefaultFileDownloader(proxy));
     }
 
     public YarnInstaller getYarnInstaller(ProxyConfig proxy) {
-        return new YarnInstaller(getInstallConfig(), new DefaultArchiveExtractor(),
-            new DefaultFileDownloader(proxy));
+        return new YarnInstaller(getInstallConfig(), new DefaultArchiveExtractor(), new DefaultFileDownloader(proxy));
     }
-
+    
     public BowerRunner getBowerRunner(ProxyConfig proxy) {
         return new DefaultBowerRunner(getExecutorConfig(), proxy);
-    }
+    }    
 
     public JspmRunner getJspmRunner() {
         return new DefaultJspmRunner(getExecutorConfig());
@@ -52,11 +46,10 @@ public final class FrontendPluginFactory {
     }
 
     public YarnRunner getYarnRunner(ProxyConfig proxy, String npmRegistryURL) {
-        return new DefaultYarnRunner(new InstallYarnExecutorConfig(getInstallConfig()), proxy,
-            npmRegistryURL);
+        return new DefaultYarnRunner(new InstallYarnExecutorConfig(getInstallConfig()), proxy, npmRegistryURL);
     }
 
-    public GruntRunner getGruntRunner() {
+    public GruntRunner getGruntRunner(){
         return new DefaultGruntRunner(getExecutorConfig());
     }
 
@@ -64,15 +57,15 @@ public final class FrontendPluginFactory {
         return new DefaultEmberRunner(getExecutorConfig());
     }
 
-    public KarmaRunner getKarmaRunner() {
+    public KarmaRunner getKarmaRunner(){
         return new DefaultKarmaRunner(getExecutorConfig());
     }
 
-    public GulpRunner getGulpRunner() {
+    public GulpRunner getGulpRunner(){
         return new DefaultGulpRunner(getExecutorConfig());
     }
 
-    public WebpackRunner getWebpackRunner() {
+    public WebpackRunner getWebpackRunner(){
         return new DefaultWebpackRunner(getExecutorConfig());
     }
 
@@ -81,8 +74,7 @@ public final class FrontendPluginFactory {
     }
 
     private InstallConfig getInstallConfig() {
-        return new DefaultInstallConfig(this.installDirectory, this.workingDirectory, this.cacheResolver,
-            defaultPlatform);
+        return new DefaultInstallConfig(installDirectory, workingDirectory, cacheResolver, defaultPlatform);
     }
 
     private static final CacheResolver getDefaultCacheResolver(File root) {
